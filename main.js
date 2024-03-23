2974d57c44343a932d3a695022c1323766f17451
FJanza <franciscojanza001@gmail.com>
2024-03-23 16:38:51 -0300

    <feat>[dev] fix worker not work correctly

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ import "./style.css";
 import Split from "split-grid";
 import {encode, decode} from "js-base64";
 import * as monaco from "monaco-editor";
-import HtmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
+
 import CssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
+import HtmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
+import JsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
 window.MonacoEnvironment = {
   getWorker: (_, label) => {
@@ -82,7 +84,6 @@ const DEFAULT_EDITOR_SETTINGS = {
   fontSize: 16,
   theme: "vs-dark",
   padding: {top: 24},
-  codeLens: true,
 };
 
 const htmlEditor = monaco.editor.create($html, {
@@ -97,7 +98,7 @@ const cssEditor = monaco.editor.create($css, {
 });
 const jsEditor = monaco.editor.create($js, {
   value: js,
-  language: "javascript",
+  language: "javaScript",
   ...DEFAULT_EDITOR_SETTINGS,
 });
 
